d4da5c61ebf88d67b2a56bb1b8c80e6274aa571d
Kiran Joy <kiranjoysebastian@gmail.com>
2016-11-27 17:05:12 +0800

    Refactor db ops to Db class

--- app.rb
+++ app.rb
@@ -1,39 +1,21 @@
 require 'sinatra'
-require 'influxdb'
+require './db'
 
 class KVStore < Sinatra::Application
 
-  @@db = InfluxDB::Client.new 'store'
-  @@series = 'kv'
+  @@db = Db.new
 
   get '/:key' do
     key = params['key']
     timestamp = params['timestamp'] || Time.now.to_i
-    fetch_val key, timestamp
+    @@db.fetch_val key, timestamp
   end
 
   post '/:key' do
     request.body.rewind
     key = params['key']
     val = request.body.read
-    write_val(key, val)
+    @@db.write_val(key, val)
   end
 
-  def fetch_val(key, timestamp)
-    result = @@db.query "select last(value) from #{@@series} where \"key\"='#{key}' and time < #{timestamp}s"
-    if result.first["values"].first
-      result.first["values"].first["last"]
-    end
-  end
-
-  def write_val(key, val)
-    cur_val = fetch_val key, Time.now.to_i
-    if cur_val != val
-      data = {
-        values: { value: val },
-        tags: { key: key }
-      }
-      @@db.write_point(@@series, data, 's')
-    end
-  end
 end
---
+++ db.rb
@@ -0,0 +1,30 @@
+require 'influxdb'
+
+class Db
+
+  DB_NAME = 'store'
+  DB_SERIES = 'kv'
+
+  def initialize
+    @db = InfluxDB::Client.new DB_NAME
+  end
+
+  def fetch_val(key, timestamp)
+    result = @db.query "select last(value) from #{DB_SERIES} where \"key\"='#{key}' and time < #{timestamp}s"
+    if result.first["values"].first
+      result.first["values"].first["last"]
+    end
+  end
+
+  def write_val(key, val)
+    cur_val = fetch_val key, Time.now.to_i
+    if cur_val != val
+      data = {
+        values: { value: val },
+        tags: { key: key }
+      }
+      @db.write_point(DB_SERIES, data, 's')
+    end
+  end
+
+end
